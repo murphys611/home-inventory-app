735465d3e79a2595d742eacbf9135d58b91813b1
murphys611 <murphys611@gmail.com>
2026-04-20 19:36:36 -0400

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const SUPABASE_URL = 'https://khzchplvymixembvydpb.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_9aw9hIt1YiATc4tBR3QHag_hSMqlD9a';
+const SUPABASE_ANON_KEY = 'REPLACE WITH SUPABASE ANON KEY';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
